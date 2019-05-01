9c341b891ea90293e4837592a2a6f1eb06dd564f
Xiaobin Gao <44366966+Xiaobin-Gao@users.noreply.github.com>
2019-05-01 07:29:59 -0400

    Update hot-news.component.ts

--- Angular_JobsWebApp/src/app/hot-news/hot-news.component.ts
+++ Angular_JobsWebApp/src/app/hot-news/hot-news.component.ts
@@ -5,7 +5,7 @@ import { Article } from './news.model';
 @Component({
   selector: 'app-hot-news',
   templateUrl: './hot-news.component.html',
-  styleUrls: ['./hot-news.component.css']
+//   styleUrls: ['./hot-news.component.css']
 })
 export class HotNewsComponent implements OnInit {
   @HostBinding('attr.class') cssClass = 'row'; votes: number;
