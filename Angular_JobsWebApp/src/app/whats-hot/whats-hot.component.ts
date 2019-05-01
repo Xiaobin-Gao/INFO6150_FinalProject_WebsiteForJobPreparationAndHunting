51418b7a784826e861c05b2adc338a80d46da60e
Xiaobin Gao <44366966+Xiaobin-Gao@users.noreply.github.com>
2019-05-01 07:31:41 -0400

    Update whats-hot.component.ts

--- Angular_JobsWebApp/src/app/whats-hot/whats-hot.component.ts
+++ Angular_JobsWebApp/src/app/whats-hot/whats-hot.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-whats-hot',
   templateUrl: './whats-hot.component.html',
-  styleUrls: ['./whats-hot.component.css']
+//   styleUrls: ['./whats-hot.component.css']
 })
 export class WhatsHotComponent implements OnInit {
 
