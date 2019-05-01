2cb613d5705054c873fadbef09f283fe85aa001e
Xiaobin Gao <44366966+Xiaobin-Gao@users.noreply.github.com>
2019-05-01 07:31:17 -0400

    Update learning-skills.component.ts

--- Angular_JobsWebApp/src/app/learning-skills/learning-skills.component.ts
+++ Angular_JobsWebApp/src/app/learning-skills/learning-skills.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-learning-skills',
   templateUrl: './learning-skills.component.html',
-  styleUrls: ['./learning-skills.component.css']
+//   styleUrls: ['./learning-skills.component.css']
 })
 export class LearningSkillsComponent implements OnInit {
 
